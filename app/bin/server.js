a7cbde3bb9aa9f3909392c86e37368a4754aa27e
Wes Bos <wesbos@gmail.com>
2012-02-05 19:49:04 -0500

    setting up client side API

--- app/bin/server.js
+++ app/bin/server.js
@@ -1,29 +1,37 @@
 #!/usr/bin/env node
 
-var express = require("express"),
-		app = express.createServer(),
-		io = require("socket.io").listen(app);
-		// socket.io may listen to an http
-		// or express server
+var express = require('express'),
+    app = express.createServer(),
+    path = require('path'),
+    io = require('socket.io').listen(app);
+    // socket.io may listen to an http
+    // or express server
 
 // Use Express to serve static content, such as our index.html
 app.configure(function(){
-	app.use(express.static(__dirname + "/../public"));
+  app.use(express.static(__dirname + '/../public'));
+});
+
+// serve up our platoon lib for development
+app.get('/platoon.js', function(req, res){
+  var url = __dirname + '/../../lib/platoon.js';
+  url = path.normalize(url);
+  res.sendfile(url);
 });
 
 //Socket.io emits this event when a connection is made.
-io.sockets.on("connection", function (socket) {
+io.sockets.on('connection', function (socket) {
 
-	// Emit a message to send it to the client.
-	socket.emit("ping", { msg: "Hello. I know socket.io." });
+  // Emit a message to send it to the client.
+  socket.emit('ping', { msg: 'Hello. I know socket.io.' });
 
-	// Print messages from the client.
-	socket.on("pong", function (data) {
-		console.log(data.msg);
-	});
+  // Print messages from the client.
+  socket.on('pong', function (data) {
+    console.log(data.msg);
+  });
 
 });
 
 app.listen(8080);
 
-console.log("socket.io server started on port 8080");
+console.log('socket.io server started on port 8080');
